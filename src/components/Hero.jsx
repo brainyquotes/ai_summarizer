cdd1f0dc590ba0990c8241688cce34e5de8a3c96
emjay-tech <142721665+emjay-tech@users.noreply.github.com>
2023-11-05 05:03:34 -0800

    added handleSubmit event handler functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
                 <span className='orange_gradient'>OpenAI GPT-4</span>
             </h1>
         <h2 className="desc">
-            Simplify your reading with Sumz, an open-source article summarizer that transforms 
+            Summarise and Simplify your reading with this Open-source article summarizer that transforms 
             lengthy articles into clear and concise summaries
 
         </h2>
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,14 +3,23 @@ import {useState, useEffect} from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
 
 const Summary = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  //event handler to fetch from the API
+  const handleSubmit = async (e) => {
+    alert('submitted')
+  }
+
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/*search*/}
       <div className="flex flex-col w-ful gap-2">
         <form action="" className="relative flex justify-center items-center"
-          onSubmit={() => {
-
-          }}
+          onSubmit = {handleSubmit}
         >
           <img src={linkIcon} alt="link-icon" 
           className='absolute left-0 my-2 ml-3 w-5'/>
@@ -18,8 +27,11 @@ const Summary = () => {
           <input 
             type="text" 
             placeholder='Paste or Enter url'
-            value=''
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({
+              ...prevArticle, //remember the previous article
+              url: e.target.value //update the url
+            })}
             required
             className='url_input peer-focus:border-gray-700 peer-focus:text-gray-700'
           />
